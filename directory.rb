081a3dce81d9230f6e9b42031f5d2381b11f62be
Celia <celiafrom12@gmail.com>
2019-05-20 15:08:00 +0100

    Modified program showing index before name

--- directory.rb
+++ directory.rb
@@ -12,6 +12,19 @@ students = [
   {name: "Norman Bates", cohort: :november}
 ]
 
+def input_students
+  puts "Please enter the name of the students"
+  puts "To finish, just hit return twice"
+  students = []
+  name = gets.chomp
+  while !name.empty? do
+    students << {name: name, chohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
+
 def print_header #using methods to make the code re-usable.
   puts "The students of Villains Academy"
   puts "-------------"
@@ -27,6 +40,7 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
