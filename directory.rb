c27f95b35ae11a3fcace9b0be117d3bfba48f908
Celia <celiafrom12@gmail.com>
2019-05-23 14:28:21 +0100

    Adding an interactive menu

--- directory.rb
+++ directory.rb
@@ -32,6 +32,30 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students".center(80)
 end
 
+def interactive_menu
+  students = [] #Outside the loop so it continue accross different iteratopms
+  loop do
+    puts "What would you like to do?"
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+      case selection
+      when "1"
+        students = input_students
+      when "2"
+        print_header
+        print(students)
+        print_footer(students)
+      when "9"
+        exit
+      else
+        puts "I don't know what you meant, try again"
+    end
+  end
+end
+
+interactive_menu
 students = input_students
 #nothing happens until we call the methods
 print_header
