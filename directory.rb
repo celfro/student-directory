a2801ad7e5f87954ebf302781133dc553180c866
Celia <celiafrom12@gmail.com>
2019-05-23 17:33:25 +0100

    Saving the data to a file

--- directory.rb
+++ directory.rb
@@ -1,20 +1,19 @@
+@students = []
 def input_students
   puts "Please enter the names of the students".center(80)
   puts "To finish, just hit return twice".center(80)
-  # create an empty array
-  students = []
   # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students".center(80)
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students".center(80)
     # get another name from the user
     name = gets.chomp
   end
-  # return the array of students
-  students
+  # return the array of @students
+  @students
 end
 
 def print_header
@@ -22,42 +21,70 @@ def print_header
   puts "-------------".center(80)
 end
 
-def print(students)
-  students.each do |student|
+def print_student_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)".center(80)
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students".center(80)
+def print_footer
+  puts "Overall, we have #{@students.count} great students".center(80)
 end
 
-def interactive_menu
-  students = [] #Outside the loop so it continue accross different iteratopms
-  loop do
-    puts "What would you like to do?"
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    selection = gets.chomp
-      case selection
-      when "1"
-        students = input_students
-      when "2"
-        print_header
-        print(students)
-        print_footer(students)
-      when "9"
-        exit
-      else
-        puts "I don't know what you meant, try again"
-    end
+def print_menu
+  puts "What would you like to do?"
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save students in file"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "3"
+    save_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again"
   end
 end
 
-interactive_menu
-students = input_students
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 #nothing happens until we call the methods
+interactive_menu
+input_students
 print_header
-print(students)
-print_footer(students)
+print_student_list
+print_menu
+show_students
+save_students
+print_footer
